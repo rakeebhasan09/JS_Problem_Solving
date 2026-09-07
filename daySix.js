07fa1b5dc0a86764bdda35b219291f9dc53f3d8b
MD Rakeeb Hasan <mdrakeebhasan1@gmail.com>
2026-09-07 21:56:55 +0600

    Update daySix

--- daySix.js
+++ daySix.js
@@ -6,3 +6,20 @@ const frequency = fruits.reduce((acc, fruit) => {
 }, {});
 
 console.log(frequency);
+
+
+নিচের object থেকে street, city এবং country console-এ দেখাও।
+
+const user = {
+  name: "Rakeeb",
+  email: "rakeeb@gmail.com",
+  address: {
+    street: "College Road",
+    city: "Mymensingh",
+    country: "Bangladesh"
+  }
+};
+
+console.log(user.address.street);
+console.log(user.address.city);
+console.log(user.address.country);
